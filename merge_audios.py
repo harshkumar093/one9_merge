4437fb9000bbd3abc43a4a44101537ed51c1331a
HARSH KUMAR <55792055+harshkumar093@users.noreply.github.com>
2025-07-01 16:42:01 +0530

    Added logs for debugging

--- merge_audios.py
+++ merge_audios.py
@@ -4,6 +4,7 @@ def merge_audios(audio_files, output_file="merged_output.mp3"):
     merged = AudioSegment.empty()
     for file in audio_files:
         audio = AudioSegment.from_file(file)
+        print(file, len(audio), audio.duration_seconds)
         merged += audio
     merged.export(output_file, format="mp3")
     print(f"✅ Merged audio saved as: {output_file}")
